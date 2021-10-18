32679c06adc4ad09b36a437593f117e6ae86a6b2
Nikolas Schmidt-Voigt <nikolas.schmidt-voigt@posteo.de>
2021-10-18 10:30:21 +0200

    do not wrap lines if output width can not be determined

--- src/conf.rs
+++ src/conf.rs
@@ -1,5 +1,5 @@
 pub static FORMAT_DATETIME: &str = "%F %R";
 pub static FORMAT_TIME: &str = "%R";
 pub static FORMAT_DATE: &str = "%F";
-pub static DEFAULT_WIDTH : usize = 80;
+pub static DEFAULT_WIDTH : usize = usize::MAX;
 pub static REPORT_INDENTATION : usize = 4;
