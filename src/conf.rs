e2f5a8b715bfd1fcafe9cf885352525f53deb454
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-06-12 23:08:31 -0700

    Feature gate what date-time formatter is being used

--- src/conf.rs
+++ src/conf.rs
@@ -1,7 +1,15 @@
 use chrono::Duration;
 
+#[cfg(feature = "second-precision")]
+pub static FORMAT_DATETIME: &str = "%F %T";
+#[cfg(not(feature = "second-precision"))]
 pub static FORMAT_DATETIME: &str = "%F %R";
+
+#[cfg(feature = "second-precision")]
+pub static FORMAT_TIME: &str = "%T";
+#[cfg(not(feature = "second-precision"))]
 pub static FORMAT_TIME: &str = "%R";
+
 pub static FORMAT_DATE: &str = "%F";
 pub static DEFAULT_WIDTH: usize = usize::MAX;
 pub static REPORT_INDENTATION: usize = 4;
