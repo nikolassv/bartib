1abe077e69cd45edf8760858b623c417cacb7cdc
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2025-06-13 00:34:25 -0700

    Remove unnecessary newline

--- src/conf.rs
+++ src/conf.rs
@@ -11,8 +11,8 @@ pub static FORMAT_DATETIME: &str = FORMAT_SECOND_PRECISION_DATETIME;
 #[cfg(not(feature = "second-precision"))]
 pub static FORMAT_TIME: &str = "%R";
 #[cfg(feature = "second-precision")]
-
 pub static FORMAT_TIME: &str = "%T";
+
 pub static FORMAT_DATE: &str = "%F";
 pub static DEFAULT_WIDTH: usize = usize::MAX;
 pub static REPORT_INDENTATION: usize = 4;
